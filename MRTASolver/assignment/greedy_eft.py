2fcb9d55fc8f9af1474b9b2839321b92f99c6883
acantalapie <acantalapie@unav.es>
2026-01-19 12:20:40 +0000

    Actualizar Greedy Earliest Finish

--- MRTASolver/assignment/greedy_eft.py
+++ MRTASolver/assignment/greedy_eft.py
@@ -15,10 +15,10 @@ class GreedyEarliestFinish(AssignmentPolicy):
             available = max(state.last_time[a], curr_time)
 
             dist1 = math.ceil(self.room_graph[state.last_room[a]][task.start] / self.fidelity)
-            dist2 = math.ceil(sel.room_graph[state.last_room[a]][task.end] / self.fidelity)
+            dist2 = math.ceil(self.room_graph[state.last_room[a]][task.end] / self.fidelity)
 
             t_pick = available + dist1 + self.action_time
-            t_drop = t_pick + dist_2 + self.action_time
+            t_drop = t_pick + dist2 + self.action_time
 
             if best_finish is None or t_drop < best_finish:
                 best_finish, best_a  = t_drop, a
