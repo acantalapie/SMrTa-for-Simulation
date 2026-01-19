93897ac606e48b5bd996f47ceb9e1d0c6ee8574e
acantalapie <acantalapie@unav.es>
2026-01-19 08:52:36 +0000

    MRTA Solver - Greedy Round Robin

---
+++ MRTASolver/assigment/round_robin.py
@@ -0,0 +1,26 @@
+from .base import AssignmentPolicy
+
+class RoundRobinAssigment(AssignmentPolicy):
+    """
+    Política de asignación Round-Robin.
+
+    Asigna las tareas de forma cíclica entre los agentes disponibles,
+    sin tener en cuenta:
+      - tiempos de ejecución
+      - distancias
+      - carga previa
+      - deadlines
+
+    Esta política se utiliza principalmente como:
+      - baseline estructural
+      - referencia para comparar otros métodos
+      - herramienta de depuración del pipeline (greedy + SMT)
+    """
+    def __init__(self):
+        self.counter = 0
+
+    def select_agent(self, task, state, curr_time):
+        agent = self.counter % state.num_agents
+        self.counter += 1
+
+        return agent
