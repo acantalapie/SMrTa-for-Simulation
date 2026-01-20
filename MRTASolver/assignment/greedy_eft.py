323bc9e0972e70e80849839b960252cfa046712e
acantalapie <acantalapie@unav.es>
2026-01-20 09:27:24 +0000

    Correción dist2 a dist(task.start, task.end)

--- MRTASolver/assignment/greedy_eft.py
+++ MRTASolver/assignment/greedy_eft.py
@@ -15,7 +15,7 @@ class GreedyEarliestFinish(AssignmentPolicy):
             available = max(state.last_time[a], curr_time)
 
             dist1 = math.ceil(self.room_graph[state.last_room[a]][task.start] / self.fidelity)
-            dist2 = math.ceil(self.room_graph[state.last_room[a]][task.end] / self.fidelity)
+            dist2 = math.ceil(self.room_graph[task.start][task.end] / self.fidelity)
 
             t_pick = available + dist1 + self.action_time
             t_drop = t_pick + dist2 + self.action_time
@@ -24,3 +24,5 @@ class GreedyEarliestFinish(AssignmentPolicy):
                 best_finish, best_a  = t_drop, a
         return best_a
 
+    def on_commit(self, agent_id, task, state):
+        state.last_room[agent_id] = task.end
