69603881550f89183e37f11b777f48841d22a8d0
acantalapie <acantalapie@unav.es>
2026-01-20 09:24:18 +0000

    Actualizacion POLICY_REGISTRY

--- MRTASolver/assignment/__init__.py
+++ MRTASolver/assignment/__init__.py
@@ -2,6 +2,6 @@ from .greedy_eft import GreedyEarliestFinish
 from .round_robin.py import RoundRobinAssigment
 
 POLICY_REGISTRY = {
-    "greedy_eft" : GreedyEarliestFinish,
+    "greedy_earliest_finish" : GreedyEarliestFinish,
     "round_robin" : RoundRobinAssigment,
 }
