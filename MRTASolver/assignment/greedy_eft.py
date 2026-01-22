51b279b0397677c8479e76cdd50778db607ba397
Ana María Cantalapiedra Arellano <acantalapie@unav.es>
2026-01-22 10:07:09 +0100

    Incluir deadline en Greedy Earliest Finish

--- MRTASolver/assignment/greedy_eft.py
+++ MRTASolver/assignment/greedy_eft.py
@@ -3,14 +3,14 @@ import math
 
 class GreedyEarliestFinish(AssignmentPolicy):
 
-    def __init__(self, room_graph, fidelity, action_time. **_):
+    def __init__(self, room_graph, fidelity, action_time, **_):
         self.room_graph = room_graph
         self.fidelity = fidelity
         self.action_time = action_time
     
-    def select_agent(self, task, state, curr_time):
+    def select_agent(self, task, state, curr_time, deadline):
         best_a, best_finish = None, None
-
+        
         for a in range(state.num_agents):
             available = max(state.last_time[a], curr_time)
 
@@ -20,8 +20,18 @@ class GreedyEarliestFinish(AssignmentPolicy):
             t_pick = available + dist1 + self.action_time
             t_drop = t_pick + dist2 + self.action_time
 
+            # Filtro duro por deadline
+            if t_drop > deadline:
+                continue
+
             if best_finish is None or t_drop < best_finish:
                 best_finish, best_a  = t_drop, a
+
+        if best_a is None:
+            raise ValueError(
+                f"Tarea {task.id} imposible: ningún agente cumple deadline={deadline}"
+            )
+        
         return best_a
 
     def on_commit(self, agent_id, task, state):
